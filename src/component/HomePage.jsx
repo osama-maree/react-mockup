44e52eee99802d1162418e12502ec1c8a34ed48d
osama maree <osamamarei2001@gmail.com>
2023-09-03 14:09:32 +0300

    Add Middle Component

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -1,44 +1,17 @@
-import React from "react";
-import { BsArrowLeft, BsThreeDots } from "react-icons/bs";
-import { MdOutlineNotificationAdd } from "react-icons/md";
-function ReactLogo() {
-  return (
-    <div className="reactLogo">
-      <BsArrowLeft />
-      <div>
-        <h3 className="logo">React</h3>
-        <small>2,611 posts</small>
-      </div>
-    </div>
-  );
-}
-function Profile() {
-  return (
-    <div>
-      <div className="imgBox">
-        <img
-          className="coverPic"
-          src="/assets/reactPic.jpeg"
-          style={{ width: "100%" }}
-          alt=""
-        />
-        <div className="bottomIcon">
-          <img className="profileImg" src="/assets/reactLogo.png" alt="" />
-          <div className="icons">
-            <BsThreeDots className="border" />
-            <MdOutlineNotificationAdd className="border"/>
-            <h4 className="following">Following</h4>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+import ReactLogo from "./ReactLogo.jsx";
+import Profile from "./Profile.jsx";
+import Details from "./Details.jsx";
+import BottomDetails from "./BottomDetails.jsx";
+
+
+
 const HomePage = () => {
   return (
     <div className="homePage">
       <ReactLogo />
       <Profile />
+      <Details />
+      <BottomDetails />
     </div>
   );
 };
