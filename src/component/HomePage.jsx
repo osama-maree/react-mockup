50d1817fcf888c789248aed0804f77eaada290a1
osama maree <osamamarei2001@gmail.com>
2023-09-03 08:58:44 +0300

    Add Profile Pic and Cover Pic

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BsArrowLeft } from "react-icons/bs";
+import { BsArrowLeft, BsThreeDots } from "react-icons/bs";
+import { MdOutlineNotificationAdd } from "react-icons/md";
 function ReactLogo() {
   return (
     <div className="reactLogo">
@@ -11,10 +12,33 @@ function ReactLogo() {
     </div>
   );
 }
+function Profile() {
+  return (
+    <div>
+      <div className="imgBox">
+        <img
+          className="coverPic"
+          src="/assets/reactPic.jpeg"
+          style={{ width: "100%" }}
+          alt=""
+        />
+        <div className="bottomIcon">
+          <img className="profileImg" src="/assets/reactLogo.png" alt="" />
+          <div className="icons">
+            <BsThreeDots className="border" />
+            <MdOutlineNotificationAdd className="border"/>
+            <h4 className="following">Following</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 const HomePage = () => {
   return (
     <div className="homePage">
       <ReactLogo />
+      <Profile />
     </div>
   );
 };
