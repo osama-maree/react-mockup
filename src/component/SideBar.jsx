64339c73f6d0b7294f6d6d0a3f0cbb13499162b2
osama maree <osamamarei2001@gmail.com>
2023-09-03 20:02:29 +0300

    Add SideBar

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
-
+import SideBarBox from "./SideBarBox.jsx";
+import {
+  AiOutlineSearch,
+  AiOutlineHome,
+  AiOutlineUsergroupDelete,
+} from "react-icons/ai";
+import { HiOutlineMail, HiOutlineUser } from "react-icons/hi";
+import { RiNotification4Line } from "react-icons/ri";
+import { BsCardList, BsBookmark } from "react-icons/bs";
+import { CiCircleMore } from "react-icons/ci";
+const data = [
+  { icon: <AiOutlineHome />, title: "Home" },
+  { icon: <AiOutlineSearch />, title: "Explore" },
+  { icon: <RiNotification4Line />, title: "Notifications" },
+  { icon: <HiOutlineMail />, title: "Messages" },
+  { icon: <BsCardList />, title: "Lists" },
+  { icon: <BsBookmark />, title: "Bookmarks" },
+  { icon: <AiOutlineUsergroupDelete />, title: "Communities" },
+  { icon: <HiOutlineUser />, title: "Profile" },
+  { icon: <CiCircleMore />, title: "More" },
+];
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div>
+      <img src="/assets/logo.jpg" className="imgLogo" alt="" />
+      {data.map((e, indx) => (
+        <SideBarBox item={e} key={indx} />
+      ))}
+      <div className="btn">Post</div>
+    </div>
+  );
 };
 
 export default SideBar;
