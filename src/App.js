22e22a2defef27901e53c7cfce632bd50431fd45
osama maree <osamamarei2001@gmail.com>
2023-09-03 07:53:00 +0300

    Add Base Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import Home from "./component/Home.jsx";
 
 function App() {
-  return <div className="App">osma</div>;
+  return <div className="App">
+   <Home/>
+  </div>
 }
 
 export default App;
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import SideBar from './SideBar.jsx'
+import HomePage from './HomePage.jsx'
+import RightSideBar from './RightSideBar.jsx'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <SideBar/>
+      <HomePage />
+      <RightSideBar />
+    </div>
   )
 }
 
